41d43e1be2d4139f6ec4c307312e7697682387cf
Artem Altukhov <aaltukhov@acumatica.com>
2017-05-22 19:03:10 +0300

    second commit

--- AsciiCharSequence/AsciiCharSequence/Program.cs
+++ AsciiCharSequence/AsciiCharSequence/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,61 @@ namespace AsciiCharSequence
     {
         static void Main(string[] args)
         {
+            byte[] mas = new byte[] { 99,100,101,102,103 };
+            AsciiCharSequence inst = new AsciiCharSequence(mas);
+
+            foreach (char item in inst.SubSequence(1,1))
+            {
+
+            }
+
+            foreach (char item in inst)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    class AsciiCharSequence: IEnumerable<char>
+    {
+        private byte[] arrayOfBytes { get; set; }
+
+        public AsciiCharSequence(byte[] mas)
+        {
+            arrayOfBytes = mas;
+        }
+
+        
+        public int Length()
+        {
+            return arrayOfBytes.Length;
+        }
+
+        public char CharAt(int index)
+        {
+            return Convert.ToChar(arrayOfBytes[index]);
+        }
+
+        public IEnumerable<char> SubSequence(int start, int end)
+        {
+            for (int i = start; i < end; i++)
+            {
+                yield return Convert.ToChar(arrayOfBytes[i]);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return arrayOfBytes.GetEnumerator();
+        }
+
+        IEnumerator<char> IEnumerable<char>.GetEnumerator()
+        {
+            foreach (var item in arrayOfBytes)
+            {
+
+                yield return Convert.ToChar(item);
+            }
         }
     }
 }
